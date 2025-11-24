c6f3b4766c7a4cf4d3fc21331d6e65423f6dc5ac
oommmaarr <omammm61@gmail.com>
2025-11-24 20:58:00 +0200

    Final

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images : {
-    domains :['res.cloudinary.com' ,'images.unsplash.com']
-  }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
